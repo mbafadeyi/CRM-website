124ebf45e5e524dad2287f5cb9cee7f629e6e83e
M.B.A. Fadeyi <fadeyibajide@gmail.com>
2023-10-04 22:50:02 +0100

    Project push
    Project

---
+++ leads/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+
+from .views import (
+    AssignAgentView,
+    CategoryDetailView,
+    CategoryListView,
+    LeadCategoryUpdateView,
+    LeadCreateView,
+    LeadDeleteView,
+    LeadDetailView,
+    LeadListView,
+    LeadUpdateView,
+)
+
+app_name = "leads"
+
+urlpatterns = [
+    path("", LeadListView.as_view(), name="lead-list"),
+    path("<int:pk>/", LeadDetailView.as_view(), name="lead-detail"),
+    path("<int:pk>/update/", LeadUpdateView.as_view(), name="lead-update"),
+    path("<int:pk>/delete/", LeadDeleteView.as_view(), name="lead-delete"),
+    path("<int:pk>/assign-create/", AssignAgentView.as_view(), name="assign-agent"),
+    path(
+        "<int:pk>/category/",
+        LeadCategoryUpdateView.as_view(),
+        name="lead-category-update",
+    ),
+    path("create/", LeadCreateView.as_view(), name="lead-create"),
+    path("categories/", CategoryListView.as_view(), name="category-list"),
+    path("categories/<int:pk>/", CategoryDetailView.as_view(), name="category-detail"),
+]
